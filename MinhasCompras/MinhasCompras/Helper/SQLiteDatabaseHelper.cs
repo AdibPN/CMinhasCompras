a5d2bbf9845c1496dd4612bff9691450678cc85b
AdibPN <apaulinineto@gmail.com>
2022-11-16 15:41:06 -0300

    27:28

---
+++ MinhasCompras/MinhasCompras/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MinhasCompras.Model;
+using SQLite;
+
+namespace MinhasCompras.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public void update(Produto p)
+        {
+
+        }
+
+        public Task<Produto> getById(int id)
+        {
+            return new Produto();
+        }
+
+        public Task <List<Produto>> getAll()
+        {
+             
+
+        }
+
+        public void delete(int id)
+        {
+
+        }
+    }
+}
