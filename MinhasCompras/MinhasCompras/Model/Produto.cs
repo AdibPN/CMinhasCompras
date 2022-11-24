02debc5c2c8c734db24086cdd5e23fae0a1fe9e5
AdibPN <apaulinineto@gmail.com>
2022-11-24 15:56:50 -0300

    fazendo a model

--- MinhasCompras/MinhasCompras/Model/Produto.cs
+++ MinhasCompras/MinhasCompras/Model/Produto.cs
@@ -7,12 +7,55 @@ namespace MinhasCompras.Model
 {
     public class Produto
     {
-        [PrimaryKey, AutoIncrent]
+        string _nome;
+        double _qnt;
+
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
+
+        public string Nome
+        {
+            get => _nome;
+            set
+            {
+                if (value == null)
+                    throw new Exception("Nome inválido");
+
+                _nome = value;
+            }
+        }
+
 
         public double Preco { get; set; }
 
+        public double Qnt
+        {
+            get
+            {
+                return _qnt;
+            }
+
+
+            set
+            {
+               
+
+                if (!double.TryParse(value.ToString(), out _qnt))
+                    _qnt = 0.0;
+
+                if (value == 0)
+                    throw new Exception("Quantidade inválida");
+
+                _qnt = value;
+            }
+        }
+
+
+
+
+        public double PrecoTotal
+        {
+            get => Preco * Qnt;
+        }
     }
 }
